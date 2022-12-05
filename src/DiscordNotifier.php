1e78d785d9d2f749e1ad8374497f7fb88d79ca12
Yashveer - PHP VM <yashveersingh444444@gmail.com>
2022-12-05 23:53:26 +0530

    version 1

--- src/DiscordNotifier.php
+++ src/DiscordNotifier.php
@@ -1,16 +1,54 @@
 <?php
 
 namespace yashveersingh\laravelDiscordNotifier;
-use Illuminate\Support\ServiceProvider;
 
-class DiscordNotifier extends ServiceProvider
+use Illuminate\Support\Facades\Http;
+
+class DiscordNotifier
 {
-    public function boot()
+    private $webHook, $username;
+
+    /**
+     * @param string|null $webHook
+     */
+    public function __construct(string $webHook = null)
     {
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->webHook = is_null($webHook) ? env('DISCORD_WEBHOOK', null) : $webHook;
     }
 
-    public function register()
+    /**
+     * @param string $webHook
+     * @return $this
+     */
+    public function setWebhook(string $webHook): DiscordNotifier
     {
+        $this->webHook = $webHook;
+        return $this;
+    }
+
+    /**
+     * @param string $username
+     * @return DiscordNotifier
+     */
+    public function setUserName(string $username): DiscordNotifier
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @param string $content
+     * @return bool
+     */
+    public function send(string $content): bool
+    {
+        $data = [
+            'content' => $content
+        ];
+        if (!is_null($this->username))
+            $data['username'] = $this->username;
+        $pendingRequest = Http::asMultipart();
+        $pendingRequest = $pendingRequest->retry(2, 5);
+        $response = $pendingRequest->post($this->webHook, $data);
+        return $response->ok();
     }
 }
